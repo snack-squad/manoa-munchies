240e7e427e37395b27b0f3bbdade08fe79d0753d
tomidt <dtem0code@gmail.com>
2023-04-12 20:19:47 -1000

    BOTTOM TEXT

--- app/imports/ui/pages/UserHome.jsx
+++ app/imports/ui/pages/UserHome.jsx
@@ -1,15 +1,51 @@
 import React from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
+import { Col, Container, Row, Image, Card, Button } from 'react-bootstrap';
 
 /* After the user clicks the "SignOut" link in the NavBar, log them out and display this page. */
 const UserHome = () => (
   <Container id="user-home" className="text-center py-3">
-    <Container>Username Home</Container>
-    <Row>
-      {/* eslint-disable-next-line react/no-unescaped-entities */}
-      <Col>Today's Picks</Col>
-      <Col>Favorites</Col>
-      <Col>Everything</Col>
+    <h1 className="py-5">Your Favourites</h1>
+
+    <Row className="align-middle text-center">
+      <Col>
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="images/food1.jpg" />
+          <Card.Body>
+            <Card.Title>McDonald&apos;s</Card.Title>
+            <Card.Text>
+              BOTTOM TEXT
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+      </Col>
+
+      <Col>
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="images/food1.jpg" />
+          <Card.Body>
+            <Card.Title>McDonald&apos;s</Card.Title>
+            <Card.Text>
+              BOTTOM TEXT
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+      </Col>
+
+      <Col>
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="images/food1.jpg" />
+          <Card.Body>
+            <Card.Title>McDonald&apos;s</Card.Title>
+            <Card.Text>
+              BOTTOM TEXT
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+      </Col>
+
     </Row>
   </Container>
 );
