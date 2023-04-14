80219e7976e32c06da9563f9ef609fdf79729b77
Nicolas Steger <njsteger@hawaii.edu>
2023-04-13 20:29:49 -1000

    added vendor role

--- app/imports/ui/pages/VendorHome.jsx
+++ app/imports/ui/pages/VendorHome.jsx
@@ -13,7 +13,7 @@ const VendorHome = () => {
     // Note that this subscription will get cleaned up
     // when your component is unmounted or deps change.
     // Get access to Stuff documents.
-    const subscription = Meteor.subscribe(Restaurant.adminPublicationName);
+    const subscription = Meteor.subscribe(Restaurant.vendorPublicationName);
     // Determine if the subscription is ready
     const rdy = subscription.ready();
     // Get the Contact documents
