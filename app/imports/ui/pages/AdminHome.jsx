681a364ffba858d351af29fce32e8cfe713350e6
PaytonHAH <paytonh@hawaii.edu>
2023-04-16 17:59:18 -1000

    Update AdminHome.jsx

--- app/imports/ui/pages/AdminHome.jsx
+++ app/imports/ui/pages/AdminHome.jsx
@@ -5,41 +5,26 @@ import { useTracker } from 'meteor/react-meteor-data';
 import LoadingSpinner from '../components/LoadingSpinner';
 import RestaurantAdmin from '../components/RestaurantAdmin';
 import { Stuffs } from '../../api/stuff/Stuff';
+import RestaurantCard from '../components/RestaurantCard';
+import { Restaurant } from '../../api/restaurant/Restaurant';
 
 /* Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
 const AdminHome = () => {
   // useTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
-  const { ready } = useTracker(() => {
+  const { ready, restaurant } = useTracker(() => {
     // Note that this subscription will get cleaned up
     // when your component is unmounted or deps change.
     // Get access to Stuff documents.
-    const subscription = Meteor.subscribe(Stuffs.userPublicationName);
+    const subscription = Meteor.subscribe(Restaurant.adminPublicationName);
     // Determine if the subscription is ready
     const rdy = subscription.ready();
-    // Get the Stuff documents
-    const stuffItems = Stuffs.collection.find({}).fetch();
+    // Get the Contact documents
+    const restaurantItems = Restaurant.collection.find({}).fetch();
     return {
-      stuffs: stuffItems,
+      restaurant: restaurantItems,
       ready: rdy,
     };
   }, []);
-
-  const restaurants = [{
-    restaurant: 'Food #1', owner: 'Nicolas', tags: 'Japanese', days: 'Monday - Friday',
-    times: '2:00 PM - 11:00 PM',
-    logo: 'Future Image', specials1: 'Noodles', specials2: 'Chicken', specials3: 'Beef',
-  },
-  {
-    restaurant: 'Food #2', owner: 'Dustin', tags: 'Indian', days: 'Monday - Friday',
-    times: '2:00 PM - 11:00 PM',
-    logo: 'Future Image', specials1: 'Noodles', specials2: 'Chicken', specials3: 'Beef',
-  },
-  {
-    restaurant: 'Food #3', owner: 'Raymond', tags: 'American', days: 'Monday - Friday',
-    times: '2:00 PM - 11:00 PM',
-    logo: 'Future Image', specials1: 'Noodles', specials2: 'Chicken', specials3: 'Beef',
-  },
-  ];
 
   return (ready ? (
     <Container className="py-3">
@@ -49,7 +34,7 @@ const AdminHome = () => {
             <h2>Restaurants</h2>
           </Col>
           <Row className="px-5">
-            {restaurants.map((restaurant, index) => (<Col key={index}><RestaurantAdmin restaurant={restaurant} /></Col>)) }
+            {restaurant.map((restaurantCard) => (<Col key={restaurantCard._id}><RestaurantCard restaurantCard={restaurantCard} /></Col>))}
           </Row>
         </Col>
       </Row>
