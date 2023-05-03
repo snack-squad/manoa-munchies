66e16ce960e042a6d18fd0af1b1456eb65a9e09f
Raymond Lai <rlai@hawaii.edu>
2023-05-02 20:58:35 -1000

    unable to implement

--- app/imports/ui/pages/AddFood.jsx
+++ app/imports/ui/pages/AddFood.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Card, Col, Container, Row } from 'react-bootstrap';
-import { AutoForm, ErrorsField, SelectField, SubmitField, BoolField, TextField } from 'uniforms-bootstrap5';
+import { AutoForm, ErrorsField, SelectField, SubmitField, BoolField, TextField, AutoField } from 'uniforms-bootstrap5';
 import swal from 'sweetalert';
 import { Meteor } from 'meteor/meteor';
+import { useTracker } from 'meteor/react-meteor-data';
 import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
 import SimpleSchema from 'simpl-schema';
 import { Food } from '../../api/food/Food';
+import { Restaurant } from '../../api/restaurant/Restaurant';
+import LoadingSpinner from '../components/LoadingSpinner';
 
 // Create a schema to specify the structure of the data to appear in the form.
 const formSchema = new SimpleSchema({
@@ -33,11 +36,28 @@ const bridge = new SimpleSchema2Bridge(formSchema);
 
 /* Renders the AddStuff page for adding a document. */
 const AddFood = () => {
-
-  // On submit, insert the data.
+  const { ready, owner, storeName } = useTracker(() => {
+    // Note that this subscription will get cleaned up
+    // when your component is unmounted or deps change.
+    // Get access to Stuff documents.
+    const subscription = Meteor.subscribe(Restaurant.userPublicationName);
+    // Determine if the subscription is ready
+    const rdy = subscription.ready();
+    // Get the Contact documents
+    const storeOwner = 'john@foo.com';
+    const restaurants = Restaurant.collection.find({ owner: storeOwner }).fetch();
+    const restaurantNames = restaurants.map(a => a.restaurant);
+    const restaurantObj = restaurantNames.map(label => ({ label, value: label }));
+    console.log(restaurantObj);
+    console.log(storeOwner);
+    return {
+      owner: storeOwner,
+      storeName: restaurantNames,
+      ready: rdy,
+    };
+  }, []);
   const submit = (data, formRef) => {
-    const { foodName, tags, storeName, allergens, picture, isSpecial, dietary } = data;
-    const owner = Meteor.user().username;
+    const { foodName, tags, allergens, picture, isSpecial, dietary } = data;
     Food.collection.insert(
       { foodName, tags, storeName, allergens, picture, isSpecial, dietary, owner },
       (error) => {
@@ -49,11 +69,12 @@ const AddFood = () => {
         }
       },
     );
+
   };
 
   // Render the form. Use Uniforms: https://github.com/vazco/uniforms
   let fRef = null;
-  return (
+  return (ready ? (
     <Container className="py-3">
       <Row className="justify-content-center">
         <Col xs={5}>
@@ -71,7 +92,7 @@ const AddFood = () => {
                 </Row>
                 <Row>
                   <Col>
-                    <SelectField name="storeName" />
+                    <TextField name="storeName" />
                   </Col>
                   <Col>
                     <TextField name="allergens" />
@@ -88,7 +109,7 @@ const AddFood = () => {
         </Col>
       </Row>
     </Container>
-  );
+  ) : <LoadingSpinner />);
 };
 
 export default AddFood;
